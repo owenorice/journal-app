8b73ca73481147f0a9f3ee78e08982db711443dc
Owen Rice <owenorice+github@pm.me>
2026-07-06 14:24:55 +0100

    Name, link, and delete entries

---
+++ db/migrate/20260706114444_add_link_to_entries.rb
@@ -0,0 +1,5 @@
+class AddLinkToEntries < ActiveRecord::Migration[8.1]
+  def change
+    add_column :entries, :link, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_03_143523) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_06_114444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "entries", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.string "name"
+    t.string "link"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "tables", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "name"
-    t.datetime "updated_at", null: false
-  end
+  
 end
